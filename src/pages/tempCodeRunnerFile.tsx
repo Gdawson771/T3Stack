8cef118ea441ef0a03725b49f4659670eecea388
gareth dawson <gdawson771@gmail.com>
2023-08-30 09:56:49 +0100

    removed temp code runner file

--- src/pages/tempCodeRunnerFile.tsx
+++
@@ -1,3 +0,0 @@
- if (!user.username && user.primaryWeb3WalletId && user.web3Wallets) {
-    const primaryWallet = user.web3Wallets.filter((wallet) => wallet.id === user.primaryWeb3WalletId)[0];}
-  const IsWealthy = api.ethAPI.getBalance.useQuery({address: "0x"})
